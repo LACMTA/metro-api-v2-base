595b142365b167e6df88bfe05da53878ace56681
albertkun <albertk@gmx.com>
2023-11-15 17:32:01 -0800

    feat: added connection pooling with pgbouncer refactor: database connection and update canceled trips loading feat: [data] added keepalive

--- data-loading-service/app/update_canceled_trips.py
+++ data-loading-service/app/update_canceled_trips.py
@@ -36,17 +36,26 @@ def run_update():
     except Exception as e:
         # logger.exception('FTP transfer failed: ' + str(e))
         print('FTP transfer failed: ' + str(e))
+from sqlalchemy.orm import Session
 
 def load_canceled_service_into_db(path_to_json_file):
-    with open(path_to_json_file) as json_file:
-        opened_json_file = json.load(json_file)
-    canceled_data_frame = pd.json_normalize(data=opened_json_file['CanceledService'])
-    canceled_data_frame['trp_route'] = canceled_data_frame['trp_route'].str.replace(' ','')
-    canceled_data_frame['dty_number'] = canceled_data_frame['dty_number'].str.replace(' ','')
-    canceled_data_frame['LastUpdateDate'] = canceled_data_frame['LastUpdateDate'].str.split(';').str[0].str.replace('_',' ')
+    session = Session(bind=engine)
+    try:
+        with open(path_to_json_file) as json_file:
+            opened_json_file = json.load(json_file)
+        canceled_data_frame = pd.json_normalize(data=opened_json_file['CanceledService'])
+        canceled_data_frame['trp_route'] = canceled_data_frame['trp_route'].str.replace(' ','')
+        canceled_data_frame['dty_number'] = canceled_data_frame['dty_number'].str.replace(' ','')
+        canceled_data_frame['LastUpdateDate'] = canceled_data_frame['LastUpdateDate'].str.split(';').str[0].str.replace('_',' ')
 
-    canceled_data_frame_from_database = pd.read_sql_query('select * from "canceled_service"',con=engine)
-    canceled_data_frame_from_database = canceled_data_frame_from_database.drop_duplicates(subset=['dpce_date','m_gtfs_trip_id'], keep='first')
-    combined_df = pd.concat([canceled_data_frame_from_database,canceled_data_frame],ignore_index=True)
-    combined_df.drop_duplicates(subset=['dpce_date','m_gtfs_trip_id'], keep='first')
-    combined_df.to_sql('canceled_service',engine,index=False,if_exists="replace",schema=Config.TARGET_DB_SCHEMA)
+        canceled_data_frame_from_database = pd.read_sql_query('select * from "canceled_service"',con=session.bind)
+        canceled_data_frame_from_database = canceled_data_frame_from_database.drop_duplicates(subset=['dpce_date','m_gtfs_trip_id'], keep='first')
+        combined_df = pd.concat([canceled_data_frame_from_database,canceled_data_frame],ignore_index=True)
+        combined_df.drop_duplicates(subset=['dpce_date','m_gtfs_trip_id'], keep='first')
+        combined_df.to_sql('canceled_service',session.bind,index=False,if_exists="replace",schema=Config.TARGET_DB_SCHEMA)
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
